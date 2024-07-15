00b98f0dd94ab6412488cbbb17d700e708e5799c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-15 16:48:22 +0100

    added trim, tokenizing

--- main.c
+++ main.c
@@ -1,12 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// typedef struct {
+//     char *name;
+//     enum Accessor access_modifier;
+//     enum Classifier item_modifier;
+// } Property;
+
+// typedef struct {
+//     char *name;
+//     enum Accessor access_modifier;
+//     enum Classifier item_modifier;
+//     char *return_type;
+// } Method;
+
+// enum Accessor {
+//     PUBLIC,
+//     PROTECTED,
+//     PRIVATE,
+//     PACKAGE
+// };
+
+enum TokenType {
+    CLASS,
+    IDENTIFIER,
+    OPEN_BRACE,
+    CLOSE_BRACE
+};
 
 typedef struct {
     char *name;
-} Class ;
+    enum TokenType type;
+} Token;
+
+char* trim(char* str) {
+    while (isspace((unsigned char)*str)) 
+        str++;
+
+    if (*str == 0) 
+        return str;
+
+    char* end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end)) 
+        end--;
+
+    end[1] = '\0';
+    return str;
+}
 
 void Tokenize(char *source) {
-    
+    char* pch = strtok(source, "\r\n");
+
+    int count = 0;
+    while (pch != NULL)
+    {
+        char* trimmed = trim(pch);
+
+        if (count == 0 && !strcmp(trimmed, "classDiagram") == 0) {
+            // error, the first line should be "classDiagram"
+        }
+
+        printf("%d %s\n", count, trimmed);
+
+        pch = strtok(NULL, "\r\n");
+        count++;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -34,7 +94,6 @@ int main(int argc, char *argv[]) {
 
     fread(buff, 1, sz, fptr);
     buff[sz] = '\0';
-
     fclose(fptr);
 
     Tokenize(buff);
